5e32a8bdd3a0829d22d0d7b0279e7fcac9c822b6
Sonu Kumar Choudhury <sonu@Sonus-MacBook-Air.local>
2020-06-07 13:43:03 +0530

    Initial Commit

---
+++ okCredit/src/main/java/com/okCredit/App.java
@@ -0,0 +1,13 @@
+package com.okCredit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
